6452d41aea9738c2a237f42545f48bc8cbe169a2
domWalters <waltersdom@gmail.com>
2018-11-25 14:31:50 +0000

    Added blank creator, mutation, and duumb crossover to Player class.

--- game/src/player.rs
+++ game/src/player.rs
@@ -8,10 +8,33 @@ pub struct Player {
 
 impl Player {
 
-    pub fn reset(&mut self) {
-        self.payoff = 0.0;
+    pub fn new_blank() -> Player {
+        Player {
+            strategy: DataSlice::new_blank(),
+            payoff: 0.0,                     // dangerous
+            stocks_purchased: Vec::new(),
+        }
     }
 
+    pub fn reset(&mut self) {
+        self.payoff = 0.0;
+        self.stocks_purchased = Vec::new();
+    }
 
+    pub fn dumb_crossover(&self, player: Player) -> Player {
+        Player {
+            strategy: self.strategy.dumb_crossover(player.strategy),
+            payoff: 0.0,
+            stocks_purchased: Vec::new(),
+        }
+    }
+
+    pub fn mutate(&self, c: f64) -> Player {
+        Player {
+            strategy: self.strategy.mutate(c),
+            payoff: 0.0,
+            stocks_purchased: Vec::new(),
+        }
+    }
 
 }
